5496fe75d10cd5198c00aa52fa1d2ce2970b1ee7
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-28 17:05:34 -0400

    Definidas columnas iniciales a pcupar en la entidad Product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,5 +9,36 @@ export class Product {
   @Column('text', {
     unique: true
   })
-  title2: string;
+  title: string;
+
+  @Column('numeric', {
+    default: 0
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+  // tags
+  // images
 }
