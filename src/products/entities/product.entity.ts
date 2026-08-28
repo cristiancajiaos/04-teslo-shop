f02383fdaa69339d4746ca57e745f48c32f59767
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-28 16:57:32 -0400

    Creado Resource Products, entidad Product, y nexo con la tabla product de DB

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
+
+  @Column('text', {
+    unique: true
+  })
+  title2: string;
+}
