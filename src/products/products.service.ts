2b2ed5168d77423b01a31f260dbaa433b8775fde
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-31 12:07:18 -0400

    Añadida búsqueda por ID o por slug a la solicitud GET /products/term

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,6 +11,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/dto/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -46,12 +47,22 @@ export class ProductsService {
     }
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({
-      id: id,
-    });
+  async findOne(term: string) {
+
+    let product: Product | null;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({
+        id: term
+      });
+    } else {
+      product = await this.productRepository.findOneBy({
+        slug: term
+      });
+    }
+
     if (!product) {
-      throw new NotFoundException(`Product with ID ${id} not found`);
+      throw new NotFoundException(`Product with term ${term} not found`);
     }
     return product;
   }
