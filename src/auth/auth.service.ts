1e1a47f59ac71fb64b20b32a0724baf52c9a9198
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-01 16:39:15 -0400

    Creado DTO loginUserDto, creado método para login asociado a solicitud POST /api/auth/login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,14 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -14,23 +19,43 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const {password, ...userData} = createUserDto;
+      const { password, ...userData } = createUserDto;
       const user = this.userRepository.create({
         ...userData,
-        password: bcrypt.hashSync(password, 10)
+        password: bcrypt.hashSync(password, 10),
       });
       await this.userRepository.save(user);
-      const {password: pass, ...userInfo} = user;
+      const { password: pass, ...userInfo } = user;
       return userInfo;
     } catch (error) {
       this.handleDBErrors(error);
     }
   }
 
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Credentials are not valid (email)');
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credentials are not valid (password)');
+    }
+
+    return user;
+    //TODO: Retornar JWT
+  }
+
   private handleDBErrors(error: any) {
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
-    } 
+    }
 
     console.log(error);
 
