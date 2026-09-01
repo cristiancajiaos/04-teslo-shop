d39f13b23d9bf974dfc61f0d98eb99301f6a71cb
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-01 17:30:09 -0400

    Creado módulo de autenticación

---
+++ src/auth/strategies/jwt-strategy.ts
@@ -0,0 +1,41 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt, StrategyOptionsWithRequest, StrategyOptions } from 'passport-jwt';
+import { User } from '../entities/user.entity';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly configService: ConfigService
+  ) {
+    const options: StrategyOptions = {
+      secretOrKey: configService.get('JWT_SECRET')!,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+
+    }
+    super(options);
+  }
+  async validate(payload: JwtPayload): Promise<User> {
+    const {email} = payload;
+
+    const user = await this.userRepository.findOneBy({email});
+    
+    if (!user) {
+      throw new UnauthorizedException('Token not valid');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('User is inactive, talk with an admin');
+    }
+
+    return user;
+  }
+}
