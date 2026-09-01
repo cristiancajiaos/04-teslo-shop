2591bb65a73f9b47d36aaaba6c50420845e4c16d
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-01 16:10:12 -0400

    Instalado bcrypt, se encripta la contraseña, y no se devuelve al consultar la solicitud POST /api/auth/register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,9 +14,14 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const {password, ...userData} = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10)
+      });
       await this.userRepository.save(user);
-      return user;
+      const {password: pass, ...userInfo} = user;
+      return userInfo;
     } catch (error) {
       this.handleDBErrors(error);
     }
