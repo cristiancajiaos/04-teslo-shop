eaeb0a0ceff20e57a0bf57bf5da9472765750be0
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-01 15:50:15 -0400

    Creado método para crear usuario en el AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save(user);
+      return user;
+    } catch (error) {
+
+    }
   }
 }
