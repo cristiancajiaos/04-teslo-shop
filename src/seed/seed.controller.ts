5d2bd08e4d0908d28e6ced5499f1b608cb199d84
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-02 12:44:24 -0400

    Importado decorador compuesto en seed

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
+  @Auth(ValidRoles.admin)
   findAll() {
     return this.seedService.runSeed();
   }
