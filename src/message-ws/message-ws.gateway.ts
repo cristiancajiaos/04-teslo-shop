84f57e74397e19f503046ae19010d4e6c107a14c
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-03 14:55:24 -0400

    Al conectar o desconectar usuario se manda la lista de clientes conectados para mostrar

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,20 +1,25 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 
 @WebSocketGateway({cors: true, namespace: '/'})
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
+
   constructor(private readonly messageWsService: MessageWsService) {}
   
   handleConnection(client: Socket) {
     this.messageWsService.registerClient(client);
     console.log({conectados: this.messageWsService.getConnectedClients()});
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
     this.messageWsService.removeClient(client.id);
     console.log({conectados: this.messageWsService.getConnectedClients()});
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
 
   
