9a4f6b30ff1383281496339c6d433a5f456f9f48
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-03 15:20:18 -0400

    Habilitado servidor para que escuche un socket, creado DTO newMessageDto

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,6 +1,7 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
 
 
 @WebSocketGateway({cors: true, namespace: '/'})
@@ -12,14 +13,20 @@ export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnec
   
   handleConnection(client: Socket) {
     this.messageWsService.registerClient(client);
-    console.log({conectados: this.messageWsService.getConnectedClients()});
+    // console.log({conectados: this.messageWsService.getConnectedClients()});
     this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
     this.messageWsService.removeClient(client.id);
-    console.log({conectados: this.messageWsService.getConnectedClients()});
+    // console.log({conectados: this.messageWsService.getConnectedClients()});
     this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+  }
+
+  //message-from-client
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log(client.id, payload);
   }
 
   
