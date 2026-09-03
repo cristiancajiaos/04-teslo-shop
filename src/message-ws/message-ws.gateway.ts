a96fd3bc2c9781de37f4777d7a53332d8c9179e1
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-03 14:37:40 -0400

    Al entrar o salir un cliente se muestra el número de clientes conectados

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -8,10 +8,14 @@ export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnec
   constructor(private readonly messageWsService: MessageWsService) {}
   
   handleConnection(client: Socket) {
-    console.log('Client connected', client.id)
+    this.messageWsService.registerClient(client);
+    console.log({conectados: this.messageWsService.getConnectedClients()});
   }
 
   handleDisconnect(client: Socket) {
-    console.log('Client disconnected', client.id);
+    this.messageWsService.removeClient(client.id);
+    console.log({conectados: this.messageWsService.getConnectedClients()});
   }
+
+  
 }
